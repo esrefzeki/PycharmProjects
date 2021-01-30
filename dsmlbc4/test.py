8ec1c11b38688a93dd20552d0b50d0983549598b
Esref Zeki PARLAK <54935247+esrefzeki@users.noreply.github.com>
2021-01-30 14:48:22 +0300

    Add files via upload

--- dsmlbc4/test.py
+++ dsmlbc4/test.py
@@ -1,3 +1,16 @@
 # TEST ANYTHING THIS LINE BELOW
 # ------------------------------------
+import numpy as np
+import pandas as pd
+df = pd.DataFrame(np.random.randint(1, 4, size=(50000, 8)),
+                  columns=list('ABCDEFGH'))
 
+df = df.apply(lambda x: x.map({1: "A", 2: "B", 3: "C"}), axis=1)
+df = df.astype("category")
+
+for col in df.columns:
+    print(df[col].value_counts())
+
+df.head()
+
+# TESTING FOR GITHUB
